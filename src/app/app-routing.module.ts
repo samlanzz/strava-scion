efb94800ed0a83d66e8f123d23fc49fa51244a45
Lanz Valentina (IT-SWE-CC2-UI) <lanz.valentina@sbb.ch>
2019-09-09 09:22:05 +0200

    Initial setup of workbench

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PersonListComponent } from './person-list/person-list.component';
+import { PersonComponent } from './person/person.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'persons', component: PersonListComponent},
+  {path: 'persons/:id', component: PersonComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { WorkbenchModule } from '@scion/workbench';
+import { PersonListComponent } from './person-list/person-list.component';
+import { PersonComponent } from './person/person.component';
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonListComponent,
+    PersonComponent
   ],
   imports: [
+    WorkbenchModule.forRoot(),
+    RouterModule, // module required by SCION Workbench
+    BrowserAnimationsModule, // module required by SCION Workbench
     BrowserModule,
     AppRoutingModule
   ],
