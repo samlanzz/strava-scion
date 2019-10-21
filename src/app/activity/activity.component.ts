a7c092b743f9695607727ce4510169eceb0e015c
vlanz <vlanz_94@hotmail.it>
2019-10-21 15:23:39 +0200

    Show activity metadata

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -5,6 +5,16 @@ import { concatMap } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 import { StravaActivity } from '../models/strava-activity';
 
+class DetailData {
+  title: string;
+  value: any;
+
+  constructor(title: string, value: any) {
+    this.title = title;
+    this.value = value;
+  }
+}
+
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
@@ -13,6 +23,7 @@ import { StravaActivity } from '../models/strava-activity';
 export class ActivityComponent implements OnInit {
 
   activity: StravaActivity;
+  detailData: DetailData[] = [];
 
   constructor(private route: ActivatedRoute,
               private stravaApiService: StravaApiService,
@@ -27,6 +38,17 @@ export class ActivityComponent implements OnInit {
       console.log(activity);
       this.view.title = activity.name;
       this.view.heading = new Date(activity.start_date).toLocaleDateString();
+      this._setDetailData();
+    });
+  }
+
+  private _setDetailData() {
+    Object.keys(this.activity).forEach(key => {
+      if (this.activity[key] instanceof Array) {
+        this.detailData.push(new DetailData(key, this.activity[key].length));
+      } else if (!(this.activity[key] instanceof Object)) {
+        this.detailData.push(new DetailData(key, this.activity[key]));
+      }
     });
   }
 
