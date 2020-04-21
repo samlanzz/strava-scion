9c253cd6fa08859059c938f2cd9a5bc8faf0c6c1
vlanz <17550954+vlanz@users.noreply.github.com>
2020-04-21 14:16:13 +0200

    chore(update): update angular core

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -6,7 +6,7 @@ export const FOR_ROOT_OPTIONS_TOKEN = new InjectionToken<any>('authConfig');
 @NgModule({
 })
 export class AuthModule {
-  static forRoot(authConfig: AuthConfig): ModuleWithProviders {
+  static forRoot(authConfig: AuthConfig): ModuleWithProviders<AuthModule> {
     return {
       ngModule: AuthModule,
       providers: [{provide: FOR_ROOT_OPTIONS_TOKEN, useValue: authConfig}]
